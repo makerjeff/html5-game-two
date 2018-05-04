3c182800f24a3c60997dd156e1c64eec670d3b92
Jeff Wu <makerjeffwu@gmail.com>
2018-05-03 20:47:58 -0700

    continue working

--- public/js/game.js
+++ public/js/game.js
@@ -25,7 +25,6 @@ let game = {
 
 // --- levels object ---
 let levels = {
-
   data: [
     {
       // first level
@@ -64,7 +63,44 @@ let levels = {
 
   // Load all data and images for a specific level
   load: function() {},
+};
 
+
+// --- loader objec t---
+let loader = {
+  loaded: true,
+  loaded_count: 0,  // loaded so far
+  total_count: 0,   // total needed to load
+
+  init: function() {},
+
+  load_image: function(url) {
+    this.total_count++;
+    this.loaded = false;
+    $('#loadingscreen').show();
+
+    let image = new Image();
+    image.src = url;
+    image.onload = loader.item_loaded;
+    return image;
+  },
+
+  sound_file_extension: '.ogg',
+
+  load_sound: function(url) {
+    this.total_count++;
+    this.loaded = false;
+    $('#loadingscreen').show();
+
+    let audio = new Audio();
+    audio.src = url+loader.sound_file_extension;
+    audio.addEventListener('canplaythrough', loader.item_loaded, false);
+    return audio;
+  },
+
+  item_loaded: function() {
+    loader.loaded_count++;
+  }
 };
 
 
